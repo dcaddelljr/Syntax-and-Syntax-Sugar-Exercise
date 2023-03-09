28ac72645c91eba6a74067370f3a3b83ed625964
Dwight Caddell <dcaddelljr@gmail.com>
2023-03-09 11:54:30 -0600

    SyntaxandSyntaxSugarExercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //My answer
+                var a = 4;
+                var myAnswer = $"The answer { ((a < 9) ? "is less than nine." : "is greater than or equal to nine.")}";
+
+            Console.WriteLine(myAnswer);
+            //The answer is less than nine.
+
+            /* Using the given answer
+            var a = 4;
+            var myAnswer = (a < 9) ? $"{a} is less than nine" : $"{a} is greater than nine";
+            Console.WriteLine(myAnswer);
+            a is less than nine.*/
+
+
         }
     }
 }
